9c599911979e4d20bd92cc46957bad375dd7c9d7
Timothy <tidee98@web.de>
2023-07-31 10:50:43 +0200

    updated mutations added query added controller implementing mutations and query

---
+++ src/main/java/de/unistuttgart/iste/gits/content_service/controller/StageController.java
@@ -0,0 +1,39 @@
+package de.unistuttgart.iste.gits.content_service.controller;
+
+import de.unistuttgart.iste.gits.content_service.service.StageService;
+import de.unistuttgart.iste.gits.generated.dto.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class StageController {
+
+    private final StageService stageService;
+
+    @MutationMapping
+    public Stage createStage(@Argument CreateStageInput input){
+        return stageService.createNewStage(input);
+    }
+
+    @MutationMapping
+    public Stage updateStage(@Argument UpdateStageInput input){
+        return stageService.updateStage(input);
+    }
+
+    @MutationMapping
+    public UUID deleteStage(@Argument UUID uuid){
+        return stageService.deleteStage(uuid);
+    }
+
+    @MutationMapping
+    public StageOrder updateStageOrder(@Argument StageOrderInput input){
+        return stageService.reorderStages(input);
+    }
+}
---
+++ src/main/java/de/unistuttgart/iste/gits/content_service/controller/WorkPathController.java
@@ -0,0 +1,42 @@
+package de.unistuttgart.iste.gits.content_service.controller;
+
+import de.unistuttgart.iste.gits.content_service.service.WorkPathService;
+import de.unistuttgart.iste.gits.generated.dto.UpdateWorkPathInput;
+import de.unistuttgart.iste.gits.generated.dto.WorkPath;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class WorkPathController {
+
+    private final WorkPathService workPathService;
+
+    @MutationMapping
+    public WorkPath createWorkPath(@Argument String name){
+        return workPathService.createWorkPath(name);
+    }
+    @MutationMapping
+    public WorkPath updateWorkPath(@Argument UpdateWorkPathInput input){
+        return workPathService.updateWorkPath(input);
+    }
+
+    @MutationMapping
+    public UUID deleteWorkPath(@Argument UUID uuid){
+        return workPathService.deleteWorkPath(uuid);
+    }
+
+    @QueryMapping
+    public List<WorkPath> getWorkPathsByChapter(@Argument UUID uuid){
+        return workPathService.getWorkPathByChapterId(uuid);
+    }
+
+}
