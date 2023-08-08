b1f32d6df1563701d1629d76a0d65452860be2bc
myluki2000 <lukas636@live.com>
2023-08-08 20:20:02 +0200

    Modify queries to match what the gateway needs

--- src/main/java/de/unistuttgart/iste/gits/content_service/persistence/repository/SectionRepository.java
+++ src/main/java/de/unistuttgart/iste/gits/content_service/persistence/repository/SectionRepository.java
@@ -2,6 +2,7 @@ package de.unistuttgart.iste.gits.content_service.persistence.repository;
 
 import de.unistuttgart.iste.gits.content_service.persistence.dao.SectionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,11 +13,5 @@ import java.util.UUID;
  */
 @Repository
 public interface SectionRepository extends JpaRepository<SectionEntity, UUID> {
-
-    /**
-     * retrieve all Section for a chapter ID
-     * @param chapterId must be non-null and UUID
-     * @return all Section having the given chapter ID
-     */
-    List<SectionEntity> findSectionEntitiesByChapterId(UUID chapterId);
+    List<SectionEntity> findByChapterIdIn(List<UUID> ids);
 }
