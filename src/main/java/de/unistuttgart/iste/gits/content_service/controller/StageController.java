ebfaa9d403f270c4fe7db36e2aceb2b274838fd0
Timothy <tidee98@web.de>
2023-08-30 14:42:20 +0200

    fixed a bug where graphql schema field for stage user data was not defined Added a graphQlAPI test to ensure endpoint working as intended Removed obsolete type in schema

--- src/main/java/de/unistuttgart/iste/gits/content_service/controller/StageController.java
+++ src/main/java/de/unistuttgart/iste/gits/content_service/controller/StageController.java
@@ -3,7 +3,8 @@ package de.unistuttgart.iste.gits.content_service.controller;
 import de.unistuttgart.iste.gits.common.user_handling.LoggedInUser;
 import de.unistuttgart.iste.gits.content_service.service.StageService;
 import de.unistuttgart.iste.gits.content_service.service.UserProgressDataService;
-import de.unistuttgart.iste.gits.generated.dto.*;
+import de.unistuttgart.iste.gits.generated.dto.Stage;
+import de.unistuttgart.iste.gits.generated.dto.UpdateStageInput;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -23,31 +24,28 @@ public class StageController {
     private final UserProgressDataService userProgressDataService;
 
     @MutationMapping
-    public Stage createStage(@Argument UUID sectionId){
+    public Stage createStage(@Argument UUID sectionId) {
         return stageService.createNewStage(sectionId);
     }
 
     @MutationMapping
-    public Stage updateStage(@Argument UpdateStageInput input){
+    public Stage updateStage(@Argument UpdateStageInput input) {
         return stageService.updateStage(input);
     }
 
     @MutationMapping
-    public UUID deleteStage(@Argument UUID id){
+    public UUID deleteStage(@Argument UUID id) {
         return stageService.deleteStage(id);
     }
 
-    public abstract class SectionResolver<T extends Stage>{
+    @SchemaMapping(field = "requiredContentsProgress")
+    public double requiredContentsProgress(Stage stage, @ContextValue LoggedInUser currentUser) {
+        return userProgressDataService.getStageProgressForUser(stage, currentUser.getId(), true);
+    }
 
-        @SchemaMapping(field = "requiredContentsProgress")
-        public double requiredContentsProgress(T stage, @ContextValue LoggedInUser currentUser){
-            return userProgressDataService.getStageProgressForUser(stage, currentUser.getId(), true);
-        }
-
-        @SchemaMapping(field = "optionalContentsProgress")
-        public double optionalContentsProgress(T stage, @ContextValue LoggedInUser currentUser){
-            return userProgressDataService.getStageProgressForUser(stage, currentUser.getId(), false);
-        }
+    @SchemaMapping(field = "optionalContentsProgress")
+    public double optionalContentsProgress(Stage stage, @ContextValue LoggedInUser currentUser) {
+        return userProgressDataService.getStageProgressForUser(stage, currentUser.getId(), false);
     }
 
 }
